3b0cd17aab25bdfb33f682f1b067fb869151d8dd
aravinthg005 <aravinth.g005@gmail.com>
2020-06-01 00:05:08 +0530

    single todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,12 @@
 class TodosController < ApplicationController
     def index
         render plain: Todo.all.to_a
+        join("\n")
+    end
+
+    def show
+        id = params[:id]
+        todo=Todo.find(id)
+        render plain: todo.to_s
     end
 end
