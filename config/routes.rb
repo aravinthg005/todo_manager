5be6d74a7db4087f176d95053ee38a9426fe5234
aravinthg005 <aravinth.g005@gmail.com>
2020-06-01 01:43:05 +0530

    created user table and added routingsand controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+  #FOR USERS
+
+  resources :users
+  post "users/login", to: "users#login"
+  get "/signin" => "sessions#new", as: :new_sessions
+  post "/signin", to: "sessions#create", as: :sessions
+  delete "/signout", to: "sessions#destroy", as: :destroy_session
+  get "/", to: "home#index"
+  
+  
+  #FOR TODOS
   get "todos", to: "todos#index" 
   post "todos/create", to: "todos#create"
   get "todos/:id", to: "todos#show"
-
   #using convention
   resources :todos
 end
