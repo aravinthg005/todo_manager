1b167e75820d8b5d2d6ff9ab9c85834c1aca576e
aravinthg005 <aravinth.g005@gmail.com>
2020-06-01 01:07:42 +0530

    added resources convention

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "todos", to: "todos#index" 
-  get "todos/create", to: "todos#create"
+  post "todos/create", to: "todos#create"
   get "todos/:id", to: "todos#show"
+
+  #using convention
+  resources :todos
 end
