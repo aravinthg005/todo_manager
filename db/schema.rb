5b41bc33c23babf24e89aba034bcf7da5aa97af9
aravinthg005 <aravinth.g005@gmail.com>
2020-06-07 22:45:56 +0530

    after deploying on heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_061630) do
+ActiveRecord::Schema.define(version: 2020_06_06_194906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "todos", force: :cascade do |t|
-    t.text "todo_text"
+    t.text "todo_txt"
     t.date "due_date"
     t.boolean "completed"
-    t.bigint "user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "last_name"
-  end
 end
