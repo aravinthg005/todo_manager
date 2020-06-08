c02e1422bc651db71b0a59f5ed1db2fb589302da
aravinthg005 <aravinth.g005@gmail.com>
2020-06-08 09:36:08 +0530

    after  heroku updation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,26 @@
 class UsersController < ApplicationController
-    skip_before_action :ensure_user_login
-    before_action :set_user, only: [:show, :edit, :update, :destroy]
-  
-    def login
-      user_email = params[:email]
-      user_password = params[:password]
-      user = User.find_by(email: user_email, password: user_password)
-      response_text = "True"
-      response_text = "False" if user == nil
-      render plain: response_text
-    end
-  
-    def create
-      user_new = User.new(
-        first_name: params[:first_name],
-        last_name: params[:last_name],
-        email: params[:email],
-        password: params[:password],
-      )
-      if user_new.save
-        session[:current_user_id] = user_new.id
-        redirect_to "/"
-      else
-        flash[:error] = user_new.errors.full_messages.join(", ")
-        redirect_to new_user_path
-      end
+  def login
+    user_email = params[:email]
+    user_password = params[:password]
+    user = User.find_by(email: user_email, password: user_password)
+    response_text = "True"
+    response_text = "False" if user == nil
+    render plain: response_text
+  end
+
+  def create
+    user_new = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
+    )
+    if user_new.save
+      session[:current_user_id] = user_new.id
+      redirect_to "/"
+    else
+      flash[:error] = user_new.errors.full_messages.join(", ")
+      redirect_to new_user_path
     end
   end
+end
